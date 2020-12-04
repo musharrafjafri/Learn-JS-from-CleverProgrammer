ea16c8ee46f8d613083192ded37147ea85ef59a6
musharrafjafri <62510647+musharrafjafri@users.noreply.github.com>
2020-12-04 19:01:26 +0500

    completed challenge 1 (Age in days.

--- Chellange 1 Age in Days/scripts/script.js
+++ Chellange 1 Age in Days/scripts/script.js
@@ -1,1 +1,9 @@
-console.log("Challenge of Age in days.")
+function ageInDays(){
+    var birthYear = Number(prompt("What is your birthyear?"))
+    var result = (2020 - birthYear) * 365
+    document.getElementById("result").innerHTML = "You are " + result + " days old.";
+}
+
+function reset(){
+    document.getElementById("result").remove();
+}
